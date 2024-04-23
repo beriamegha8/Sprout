1b7afa8a9f1b0de9c19023bd6baaa8ac7fe67527
maryam1909 <141656129+maryam1909@users.noreply.github.com>
2024-04-23 13:20:46 +0530

    Update AboutProject.java

--- sprout/AboutProject.java
+++ sprout/AboutProject.java
@@ -17,35 +17,36 @@ public class AboutProject extends JFrame{
         setSize(540,380);
         setLocation(430,280);
         setLayout(new BorderLayout());
-        
+
         ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("sprout/icons/systemIcon.png"));
         setIconImage(icon.getImage());
-        
+        Color oliveGreen = new Color(85, 107, 47);
+
         title = new JLabel("E-Learning System", JLabel.CENTER);
         title.setFont(new Font(Font.SERIF, Font.BOLD, 25));
-        title.setBackground(Color.LIGHT_GRAY);
-        title.setForeground(Color.BLUE);
+        title.setBackground(oliveGreen);
+        title.setForeground(Color.WHITE);
         title.setOpaque(true);
         add(title, BorderLayout.NORTH);
-       
-        
-        String txt = "\t-->  E-Learning System is a desktop-based Windows application "
+
+
+        String txt = "\t *  E-Learning System is a desktop-based Windows application "
                 + "developed in Java with Swing and AWT. This project aims at serving Students and Teachers in "
                 + "Online based learning. \n\n" +
-                        "\t-->  Admins can add subjects, Teachers can add courses in a particular subject and "
+                "\t* Admins can add subjects, Teachers can add courses in a particular subject and "
                 + "Students can Enroll courses and study them, and also Students can message other participants in a "
                 + "particular course except the Teacher teaching that course.";
-        
+
         aboutText = new JTextArea(txt);
         aboutText.setLineWrap(true);
         aboutText.setWrapStyleWord(true);
         aboutText.setFont(new Font(Font.DIALOG, Font.ITALIC, 19));
-        aboutText.setForeground(Color.BLACK);
-        aboutText.setBackground(Color.ORANGE);
-        aboutText.setBorder(new LineBorder(Color.BLUE, 2, true));
+        aboutText.setForeground(oliveGreen);
+        aboutText.setBackground(Color.WHITE);
+        aboutText.setBorder(new LineBorder(Color.lightGray, 2, true));
         aboutText.setEditable(false);
         add(aboutText, BorderLayout.CENTER);
-        
+
         setResizable(false);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setVisible(true);
