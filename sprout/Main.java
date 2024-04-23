3a4e5b0ea11ce1a116889b299855934307c2e568
maryam1909 <141656129+maryam1909@users.noreply.github.com>
2024-04-23 13:19:16 +0530

    Update Main.java

--- sprout/Main.java
+++ sprout/Main.java
@@ -21,6 +21,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.Timer;
+import java.awt.Dimension;
 
 public class Main extends JFrame implements ActionListener{
     JPanel headerPanel, mainPanel, footerPanel;
@@ -36,25 +37,26 @@ public class Main extends JFrame implements ActionListener{
         setLocation(35,30);
         setLayout(new BorderLayout());
         this.main = this;
-        
+
         ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("sprout/icons/systemIcon.png"));
         setIconImage(icon.getImage());
-        
+
         headerPanel = new JPanel(new BorderLayout());
         add(headerPanel, BorderLayout.NORTH);
-        
+
         mainPanel = new JPanel();
         mainPanel.setLayout(null);
+        mainPanel.setBackground(Color.WHITE);
         add(mainPanel, BorderLayout.CENTER);
-        
+
         footerPanel = new JPanel(new BorderLayout());
         add(footerPanel, BorderLayout.SOUTH);
-        
-        
+
+
         //Header Code
-        aboutProject = new JMenuItem("About Project");    
-        aboutDevelopers = new JMenuItem("About Developers");  
-        menuBar = new JMenuBar();    
+        aboutProject = new JMenuItem("About Project");
+        aboutDevelopers = new JMenuItem("About Developers");
+        menuBar = new JMenuBar();
         about = new JMenu("About");
         exit = new JMenuItem("Exit");
         // Adding Shortcut key to close the window
@@ -68,16 +70,16 @@ public class Main extends JFrame implements ActionListener{
         aboutProject.addActionListener(this);
         menuBar.add(file);
         menuBar.add(about);
-        
-        
+
+
         headerPanel.add(menuBar,BorderLayout.NORTH);
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("sprout/icons/MainTitle.png"));
-        Image i2 = i1.getImage().getScaledInstance(1280 ,96 ,Image.SCALE_DEFAULT);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("sprout/icons/header.png"));
+        Image i2 = i1.getImage().getScaledInstance(1280 ,121 ,Image.SCALE_DEFAULT);
         ImageIcon i3 =  new ImageIcon(i2);
         title = new JLabel(i3);
         headerPanel.add(title, BorderLayout.CENTER);
-        
-        
+
+
         JLabel clock = new JLabel();
         clock.setHorizontalAlignment(JLabel.RIGHT);
         DateFormat df = new SimpleDateFormat("yyyy-MMM-dd hh:mm:ss aa");
@@ -86,76 +88,72 @@ public class Main extends JFrame implements ActionListener{
         clock.setFont(new Font(Font.SERIF,Font.BOLD, 19));
         clock.setForeground(new Color(14,168,29));
         headerPanel.add(clock, BorderLayout.SOUTH);
-        
-        
+
+
         // A Swing timer fires one or more action events after a specified delay.
         Timer t = new Timer(500, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 DateFormat df = new SimpleDateFormat("yyyy-MMM-dd hh:mm:ss aa");
                 Calendar calobj = Calendar.getInstance();
-              clock.setText(df.format(calobj.getTime()));
+                clock.setText(df.format(calobj.getTime()));
             }
-         });
-         t.setRepeats(true);
-         t.setCoalesce(true);
-         t.setInitialDelay(0);
-         t.start();
-         
+        });
+        t.setRepeats(true);
+        t.setCoalesce(true);
+        t.setInitialDelay(0);
+        t.start();
+        Color oliveGreen = new Color(85, 107, 47);
+
         //Main Center Code
-        ImageIcon loginImg1 = new ImageIcon(ClassLoader.getSystemResource("sprout/icons/login.png"));
-        Image loginImg2 = loginImg1.getImage().getScaledInstance(96  ,96 ,Image.SCALE_DEFAULT);
-        ImageIcon loginImg3 =  new ImageIcon(loginImg2);
-        LoginButton = new  JButton("Login", loginImg3);
+        LoginButton = new JButton("Login");
         LoginButton.setHorizontalAlignment(JButton.CENTER);
-        LoginButton.setBounds(420,15,190,150);
-        LoginButton.setFont(new Font(Font.SERIF, Font.BOLD, 25));
-        LoginButton.setBackground(new Color(21,76,121));
-        LoginButton.setForeground(Color.WHITE);
+        LoginButton.setBounds(420, 15, 150, 70); 
+        LoginButton.setFont(new Font(Font.SERIF, Font.BOLD, 25)); 
+        LoginButton.setBackground(new Color(21, 76, 121));
+        LoginButton.setForeground(oliveGreen);
         LoginButton.setHorizontalTextPosition(JButton.CENTER);
         LoginButton.setVerticalTextPosition(JButton.BOTTOM);
-        LoginButton.addActionListener((ActionListener) this);
-        
-        
-        ImageIcon signUpImg1 = new ImageIcon(ClassLoader.getSystemResource("sprout/icons/signup.png"));
-        Image signUpImg2 = signUpImg1.getImage().getScaledInstance(96  ,96 ,Image.SCALE_DEFAULT);
-        ImageIcon signUpImg3 =  new ImageIcon(signUpImg2);
-        SignupButton = new  JButton("SignUp", signUpImg3);
+        LoginButton.addActionListener(this);
+
+        SignupButton = new JButton("SignUp");
         SignupButton.setHorizontalAlignment(JButton.CENTER);
-        SignupButton.setBounds(715,15,210,150);
+        SignupButton.setBounds(715, 15, 150, 70); 
         SignupButton.setFont(new Font(Font.SERIF, Font.BOLD, 25));
-        SignupButton.setBackground(new Color(21,76,121));
-        SignupButton.setForeground(Color.WHITE);
+        SignupButton.setBackground(new Color(21, 76, 121));
+        SignupButton.setForeground(oliveGreen);
         SignupButton.setHorizontalTextPosition(JButton.CENTER);
         SignupButton.setVerticalTextPosition(JButton.BOTTOM);
-        SignupButton.addActionListener((ActionListener) this);
-        
-        ImageIcon mainPic1 = new ImageIcon(ClassLoader.getSystemResource("sprout/icons/ElearningMain.png"));
+        SignupButton.addActionListener(this);
+
+
+        ImageIcon mainPic1 = new ImageIcon(ClassLoader.getSystemResource("sprout/icons/logonew.png"));
         Image mainPic2 = mainPic1.getImage().getScaledInstance(500 ,500 ,Image.SCALE_DEFAULT);
         ImageIcon mainPic3 =  new ImageIcon(mainPic2);
         mainScreenLbl = new JLabel(mainPic3);
-        mainScreenLbl.setBounds(422,50,500,500);
-        
-        
+        mainScreenLbl.setBounds(350,50,500,500);
+
+
         // add to mainPanel
         mainPanel.add(LoginButton);
         mainPanel.add(SignupButton);
         mainPanel.add(mainScreenLbl);
-        
+
         // Footer Panel Code
-        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("sprout/icons/footer.png"));
-        Image i5 = i4.getImage().getScaledInstance(1280 ,60 ,Image.SCALE_DEFAULT);
-        ImageIcon i6 =  new ImageIcon(i5);
-        footerLbl = new JLabel(i6);
-        footerPanel.add(footerLbl, BorderLayout.SOUTH);
-        
-         
+        JPanel footerColorPanel = new JPanel();
+        footerColorPanel.setBackground(oliveGreen); 
+        footerColorPanel.setPreferredSize(new Dimension(1280, 100));
+
+        footerPanel.add(footerColorPanel, BorderLayout.SOUTH);
+
+
+
         // Settings for the frame
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
-     @Override
+    @Override
     public void actionPerformed(ActionEvent ae){
         if(ae.getSource() == LoginButton){
             new Login();
@@ -166,12 +164,12 @@ public class Main extends JFrame implements ActionListener{
         else if (ae.getSource() == aboutDevelopers){
             new AboutDevelopers();
         }
-         else if (ae.getSource() == aboutProject){
+        else if (ae.getSource() == aboutProject){
             new AboutProject();
         }
         else if(ae.getSource() == exit){
-             System.exit(0);
-         }
+            System.exit(0);
+        }
     }
     public static void main(String[] args) {
         new Main();
