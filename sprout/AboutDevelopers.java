a55c71ab22c73406febd7fc2aab3e85ff95cf8ab
Megha Beria <beriamegha8@gmail.com>
2024-04-26 02:34:08 +0530

    Created a boiler plate Password hasher

--- sprout/AboutDevelopers.java
+++ sprout/AboutDevelopers.java
@@ -33,10 +33,9 @@ public class AboutDevelopers extends JFrame {
         title.setOpaque(true);
         add(title, BorderLayout.NORTH);
 
-        String[][] rowData = {{ "22070122116", "Maryam Bahlooli", "<html>Frontend</html>" },
+        String[][] rowData = {{ "22070122116", "Maryam Bahlooli", "<html>Frontend<br/> and Agile Diagram</html>" },
                 { "22070122117", "Megha Beria", "<html>Frontend, Database<br/> and DB Connectivity<html>" },
-                { "22070122126", "Moksh Oswal", "<html>Database Connectivity<br/> and Report</html>" },
-                { "22070122144", "Piyush Pareek", "<html>Agile Diagram</html>" } };
+                { "22070122126", "Moksh Oswal", "<html>Database Connectivity<br/> and Report</html>" }};
         String columns[] = { "PRN", "Name", "Contributions" };
 
         tablePanel = new JPanel(new BorderLayout());
---
+++ sprout/PasswordHasher.java
@@ -0,0 +1,20 @@
+package sprout;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class PasswordHasher {
+
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
